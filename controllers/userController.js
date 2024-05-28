0e23abc94276345d94343925bb8310d702abb799
Ephaste <ephasteniyonizeye@gmail.com>
2024-05-28 08:40:36 +0200

    login

--- controllers/userController.js
+++ controllers/userController.js
@@ -43,49 +43,95 @@ await sendEmail(newUser.email,"welcome message","thanks",htmlMessage)
 };
 //LOGIN
 
-export const loginUser = async (req, res) => {
-  try {
-    const user = await User.findOne({ email: req.body.email });
 
-    if (!user) {
-      return res.status(404).json({
-        message: "User not found!",
-      });
-    }
 
-    let isPasswordCorrect = await comparePassword(req.body.password, user.password);
+//Login User
+export  const loginUser = async(req, res) => {
+ const {email, password} = req.body
+//Validate request
+
+if(!email || !password){
+    res.status(400);
+    throw new Error("PLease add an email and password");  
+}
+//Check if a user exist
+const user = await User.findOne({email})
+if(!user){
+    res.status(400);
+    throw new Error("User not found please signup");  
+}
+//User exist, mow lets check if passwoed is correct
+const passwordIsCorrect = await bcrypt.compare(password, user.password)
+
+ //Generate Token
+ const token = generateToken(user._id)
+ //Send HTTP-only cookie 
+ res.cookie("token", token,{
+  path: "/", 
+  httpOnly: true,
+  expires: new Date(Date.now()+ 1000 * 86400),//1 Day
+  sameSite: "none",
+  secure: true
+ })
+
+ 
+if(user && passwordIsCorrect){
+    const {_id, name, email,phone, photo, role} =user
+    res.status(200).json({
+      messgage: "Login sucssfull",
+        _id, name, email,phone, photo, role,token
+    });
+
+}else{
+    res.status(400);
+    throw new Error("Invalid email or password  ");   
+}
+};
+// export const loginUser = async (req, res) => {
+//   try {
+//     const user = await User.findOne({ email: req.body.email });
+
+//     if (!user) {
+//       return res.status(404).json({
+//         message: "User not found!",
+//       });
+//     }
+
+//     let isPasswordCorrect = await comparePassword(req.body.password, user.password);
     
 
-    if (!isPasswordCorrect) {
-      return res.status(401).json({
-        message: "Wrong password----------->",
-      });
-    }
+//     // if (!isPasswordCorrect) {
+//     //   return res.status(401).json({
+//     //     message: "Wrong password----------->",
+//     //   });
+//     // }
+//     if (isPasswordCorrect){
 
-    // If the user is authenticated, generate a token and send a success response
-    let token = generateToken({
-      _id: user._id, // Use the correct user's ID
-      email: user.email,
-    });
+//     // If the user is authenticated, generate a token and send a success response
+//     let token = generateToken({
+//       _id: user._id, // Use the correct user's ID
+//       email: user.email,
+//     });
 
-    res.status(200).json({
-      message: "Login is successful!",
-      access_token: token,
-      user: {
-        email: user.email,
-        name: user.name,
-        phone: user.phone,
-        role: user.role,
-      },
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: "Internal server error!",
+//     res.status(200).json({
+//       message: "Login is successful!",
+//       access_token: token,
+//       user: {
+//         email: user.email,
+//         name: user.name,
+//         phone: user.phone,
+//         role: user.role,
+//       },
+//     });
+//   }
+//   } catch (error) {
+//     res.status(500).json({
+//       message: "Internal server error!",
      
-    });
-    console.log(error);
-  }
-};
+//     });
+//     console.log(error);
+//   }
+// };
 //Getting users
 export const getAll = async (req, res) => {
   try {
